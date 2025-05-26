ff077ee71e220be1d212ea8450bbf5ccf8164459
amit4raj <43353306+amit4raj@users.noreply.github.com>
2025-05-26 15:36:38 +0530

    Create admin-scripts.js

---
+++ assets/js/admin-scripts.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    // You can add JavaScript specific to your admin panel here.
+    // For now, the bulk action is handled by a direct redirect.
+    // If you were to use AJAX for printing, this file would be crucial.
+});
